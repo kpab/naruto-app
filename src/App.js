fd700049fdc7eddcc9c6be84d82cc897dd4009d5
kpab <91237200+kpab@users.noreply.github.com>
2024-10-11 16:07:47 +0900

    first
    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
+const limit = 20;
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async (page) => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, { params: { page, limit } });
+    setCharacters(result.data.characters);
+    setIsLoading(false);
+  };
+
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
+  };
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    await fetchCharacters(prevPage);
+    setPage(prevPage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="header">
+          <div className="header-content">
+            <img src="logo.png" alt="logo" className="logo" />
+          </div>
+        </div>
+        {isLoading ? (
+          <div>Now Loading...</div>
+        ) : (
+          <>
+            <h1>Naruto Characters</h1>
+            <main>
+              <div className="cards-container">
+                {characters.map((character) => (
+                  <div className="card" key={character.id}>
+                    <img
+                      src={
+                        character.images[0] != null
+                          ? character.images[0]
+                          : "dummy.png"
+                      }
+                      alt={character.name}
+                      className="card-image"
+                    />
+                    <div className="card-content">
+                      <h3 className="card-title">{character.name}</h3>
+                      <p className="card-description">
+                        {character.debut?.appearsIn ?? "-"}
+                      </p>
+                      <div className="card-footer">
+                        <span className="affiliation">
+                          {character.personal?.affiliation ?? "-"}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="pager">
+                <button
+                  disabled={page === 1}
+                  className="prev"
+                  onClick={handlePrev}
+                >
+                  Previous
+                </button>
+                <span className="page-count">{page}</span>
+                <button
+                  disabled={limit > characters.length}
+                  className="next"
+                  onClick={handleNext}
+                >
+                  Next
+                </button>
+              </div>
+            </main>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
